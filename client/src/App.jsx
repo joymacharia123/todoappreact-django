17b5131734afc64e613ba8594f17ae75fbc590b9
joymacharia <machajoy23@gmail.com>
2024-04-30 20:48:27 +0300

    created front-end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Filter from "./Components/Filter";
+import Search from "./Components/Search";
+import TodoList from "./Components/TodoList";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState([
+    { id: 0, task: "Arrange books alphabetically", status: "Active" },
+    { id: 1, task: "Try a new recipe", status: "Active" },
+    { id: 2, task: "Take a 30-minute walk", status: "Active" },
+    { id: 3, task: "Declutter desk for 20 minutes", status: "Active" },
+  ]);
+
+
+// add todo function
+  const addTodo = (data) => {
+    setTodos( [ ...todos, data={...data, id:parseInt(todos[todos.length-1].id) + 1, status:"Active"}] )
+    console.log(data)
+  }
+
+  // delete function
+  const delTodo = (id) => {
+    setTodos(todos.filter( todo => todo.id != id ))
+  }
+
+
+  // update function
+  const updateTodo = (e, id, text) => {
+    e.preventDefault()
+    // this line helps to get the current todo based on the ID called todoId in TodoList
+    const todo = todos[id]
+    const updatedUser = { ...todo, task:text, status:"Active" }
+    setTodos(todos.map(t => t.id == todo.id ? updatedUser : t))
+
+  }
+
+  const completeTodo = (e, id) => {
+
+    if(e.target.checked){
+      console.log("okay")
+      setTodos(todos.map(todo => todo.id == id ? { ...todo, status:"Completed"}: todo))
+    }
+    else
+    {
+      console.log("omo")
+      setTodos(todos.map(todo => todo.id == id ? { ...todo, status:"Active"}: todo))
+    }
+
+   
+  }
+
+  const filterTodo = (cat_value) => {
+    // setTodos(todos.filter(todo => todo.status == cat_value))
+    setTodos(todos.filter((todo) => todo.status == cat_value))
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="todo-container">
+      <Search addTodo = { addTodo } />
+      <Filter filter_todo = { filterTodo }/>
+      <TodoList todos = { todos } delTodo = { delTodo } update_todo = { updateTodo } complete_todo = { completeTodo } filter_todo = { filterTodo } />
+    </div>
+  );
 }
 
-export default App
+
+
+export default App;
